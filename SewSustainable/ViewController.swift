2cb508aa0b9d2a9ea5fbb49e9e5d7c3aee71c5a1
Sophia Liu <sophia.ch102@gmail.com>
2022-08-16 14:24:41 -0400

    Initial Commit

---
+++ SewSustainable/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SewSustainable
+//
+//  Created by Sophia Liu on 8/16/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
